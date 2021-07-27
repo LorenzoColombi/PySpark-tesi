cc03a55d826528459fc73643618f92e9152c89ba
LorenzoColombi <lorenzocolombi99@gmail.com>
2021-07-27 17:27:34 +0200

    Prototipo 2/3 (manca disegno grafici)

--- Prototipo/prototipo.py
+++ Prototipo/prototipo.py
@@ -1,7 +1,22 @@
 #ML + STRUCTURED STREAMING
 #Costruzione di un modello ML e predizione in tempo reale sfruttando lo straming strutturato
 
-#Per eseguire: $SPARK_HOME/bin/spark-submit prototipo.py localhost 9999
+#CONFIGURAZIONE PATH
+
+#Directory di salvataggio checkpoints
+checkPointsPath = "/tmp/PySpark/checkpoints"
+#Input modello ML stream da file CSV + schema nel file /schema.json
+exportedPath="/home/lorenzo/Documenti/PySpark-tesi/Prototipo/exported"
+#Lettura streaming da file (vedi variabile csvFilePath)
+csvFilePath="/home/lorenzo/Documenti/PySpark-tesi/Prototipo/exported/test/*.csv"
+#Output su file JSON (vedi variabile outputDir)
+outputDir = "/home/lorenzo/Documenti/PySpark-tesi/Prototipo/outputStreaming/"
+
+#CONFIGURAZIONE PATH
+
+
+
+#Per eseguire: $SPARK_HOME/bin/spark-submit prototipo.py 
 
 #Lo streaming da file legge tutti i file .csv nella cartella indicata e si aggiorna ogni volta che un file viene aggiunto (non quando viene modificato lo stesso file)
 
@@ -10,9 +25,12 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
 from pyspark.sql.types import *
-import json
 
 from pyspark.ml.pipeline import PipelineModel
+
+import matplotlib.pyplot as plt
+
+import json
 
 #Creo al sessione
 spark = SparkSession \
@@ -20,11 +38,13 @@ spark = SparkSession \
     .appName("StructuredNetworkWordCount") \
     .getOrCreate()
 
+#Setto una directory in cui salvare i checkpoint 
+spark.conf.set("spark.sql.streaming.checkpointLocation", checkPointsPath)
+
 #Disattivo il logging di livello INFO 
 spark.sparkContext.setLogLevel('error')
 
 #Recupero dati esportati precedentemente
-exportedPath="/home/lorenzo/Documenti/PySpark-tesi/Prototipo/exported"
 
 #Recupero il modello ML salvato
 persistedModel = PipelineModel.load(exportedPath)
@@ -34,7 +54,6 @@ with open(exportedPath+"/schema.json") as f:
     new_schema = StructType.fromJson(json.load(f))
 
 #Uso un readstream da file
-csvFilePath="/home/lorenzo/Documenti/PySpark-tesi/Prototipo/exported/test/*.csv"
 
 #Lettura dati e creazione DF (INPUT)
 csvDF = spark \
@@ -42,17 +61,26 @@ csvDF = spark \
     .schema(new_schema) \
     .csv(csvFilePath)  
 
-#Stampo i dati letti: (OUTPUT)
-query = csvDF\
+#Previsione con modello ML
+predettoDF = persistedModel.transform(csvDF).select('Elevation', 'prediction')
+
+#Esporto il DF in PANDAS
+
+#Stampo i dati letti (dopo la predizione con ML): (OUTPUT)
+query = predettoDF\
     .writeStream\
     .outputMode('update')\
     .format('console')\
     .start()
 
-#TODO previsione con modello ML e confronto 
+#Esporto il datafram in un file JSON
+queryToFile = predettoDF\
+    .writeStream\
+    .format('json')\
+    .option("path", outputDir)\
+    .start()
 
-#TODO grafico previsione del modello e dato reale
 
-
-# Await Spark Streaming termination
+# Await Spark Streaming termination (2 writeStream - query)
 query.awaitTermination()
+queryToFile.awaitTermination()
